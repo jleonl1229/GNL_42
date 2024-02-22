c34d79c602216bff1d9c365ac3d15c274a83bed1
Julen Leon Larrondo <jleon-la@c3r5s2.42urduliz.com>
2024-02-22 18:48:27 +0100

    Now it's printing two lines instead of one, but if i call the function more than 2 times it doesn't work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jleon-la <jleon-la@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:50:57 by jleon-la          #+#    #+#             */
-/*   Updated: 2024/02/21 12:55:06 by jleon-la         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:44:40 by jleon-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,56 +18,6 @@ size_t	ft_strlen(const char *length);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-// char	*print_line(char *line)
-// {
-// 	size_t	i;
-// 	size_t	flag = 0;
-// 	size_t	j;
-// 	char	*heap;
-// 	size_t	z;
-// 	size_t	h;
-
-// 	i = 0;
-// 	j = 0;
-// 	z = 0;
-// 	h = 0;
-// 	while (line[i++]) // Cuento los \n del texto
-// 	{
-// 		if (line[i] == '\n')
-// 			flag++;
-// 	}
-// 	i = 0;
-// 	while (line[i]) // consigo el numero de caracteres que hay entre el ultimo \n y el final
-// 	{
-// 		while (j != flag)
-// 		{
-// 			if (line[i] == '\n')
-// 				j++;
-// 			i++;
-// 		}
-// 		z++;
-// 		i++;
-// 	}
-// 	heap = malloc(sizeof(char) * z + 1);
-// 	i = 0;
-// 	j = 0;
-// 	while (line[i]) // Consigo meter en mi malloc los caraceres en los margenes establecidos con la z
-// 	{
-// 		while (j != flag)
-// 		{
-// 			if (line[i] == '\n')
-// 				j++;
-// 			i++;
-// 		}
-// 		if (!heap)
-// 			free(heap);
-// 		heap[h] = line[i]; // POR HACER: Me falta poner un limite en el tramo anterior para que pare cuando se encuentre otro \n o el \0 y tambien que el flag sume 1 por cada vez que se entra a la funcion para asi poder ir avanzando por el string e ir haciendo next line
-// 		h++;
-// 		i++;
-// 	}
-// 	return (heap); // Devuelvo el string que corresponde a la linea
-// }
-
 char	*get_leftover(char *line)
 {
 	char	*leftover;
@@ -76,21 +26,18 @@ char	*get_leftover(char *line)
 
 	i = 0;
 	j = 0;
+	// printf("-->FULL_Line:%s\n", line);
 	while (line[i++])
 	{
-		if (line[i] == '\n')
-		{
-			i++;
+		if (line[i++] == '\n')
 			break ;
-		}
 	}
 	leftover = malloc((ft_strlen(line) + 1) - (sizeof(char) * i));
+	if (!leftover)
+		return (NULL);
 	while (line[i])
-	{
-		leftover[j] = line[i];
-		j++;
-		i++;
-	}
+		leftover[j++] = line[i++];
+	// printf("-->Leftover:%s\n", leftover);
 	return (leftover);
 }
 
@@ -112,10 +59,10 @@ char	*cut_print(char *line)
 	cut = malloc(sizeof(char) * (i + 1));
 	if (!cut)
 		return (NULL);
-	printf("%d\n", i);
 	while (z < i)
 		cut[j++] = line[z++];
 	cut[j] = '\0';
+	// printf("-->CLEAN_Line:%s\n", cut);
 	return (cut);
 }
 
@@ -128,21 +75,15 @@ char	*get_next_line(int fd)
 
 	line = malloc (sizeof(char) * 1);
 	line[0] = '\0';
+	line = ft_strjoin(line, save);
 	while (!ft_strchr(line, '\n'))
 	{
-		// printf("line:%s\n", line);
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		// printf("buffer:%s\n", buffer);
 		buffer[bytes_read] = '\0';
 		line = ft_strjoin(line, buffer);
-		// printf("line2:%s\n", line);
-		// printf("->%c\n", line[0]);
 	}
-	// Tengo que separar esta funcion en el sobrante(caracteres de despues de \n) y lo que se imprimirá en esta linea (antes de \n)
 	save = get_leftover(line);
-	// printf("line :%s\n", line);
 	return (cut_print(line));
-	// return (NULL);
 }
 
 int	main(void)
@@ -152,7 +93,10 @@ int	main(void)
 	fd = open("archivo.txt", O_RDONLY);
 	if (fd == -1)
 		printf("Error on opening the file");
-	// get_next_line(fd);
 	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	// printf("%s", get_next_line(fd));
+	// printf("%s", get_next_line(fd));
+	// printf("%s", get_next_line(fd));
 	return (0);
 }

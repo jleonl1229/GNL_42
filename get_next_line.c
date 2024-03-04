2036dc75956b27d5a39f18aceead789bd3b7d345
Julen Leon Larrondo <jleon-la@c4r2s4.42urduliz.com>
2024-03-04 17:24:49 +0100

    fd issues handled

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*  et_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jleon-la <jleon-la@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 14:50:57 by jleon-la          #+#    #+#             */
-/*   Updated: 2024/03/01 11:33:22 by jleon-la         ###   ########.fr       */
+/*   Updated: 2024/03/04 16:55:05 by jleon-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	get_leftover(char *buffer)
 	size_t	j;
 
 	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
+	while (buffer[i] != '\0' && buffer[i] != '\n')
 		i++;
 	if (buffer[i] == '\n')
 		i++;
@@ -40,7 +40,7 @@ char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*line;
-	size_t		bytes_read;
+	int			bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -49,87 +49,43 @@ char	*get_next_line(int fd)
 		return (free(line), NULL);
 	line[0] = '\0';
 	if (ft_strlen(buffer) >= 1)
+	{
 		line = ft_strjoin(line, buffer);
-	while (!ft_strchr(buffer, '\n'))
+	}
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(buffer, '\n'))
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (free(line), NULL);
 		if (bytes_read <= 0)
 			break ;
 		line = ft_strjoin(line, buffer);
 	}
-	get_leftover(buffer);
 	if (line[0] == '\0')
 		return (free(line), NULL);
+	get_leftover(buffer);
 	return (line);
 }
-
-// void	get_leftover(char *buffer)
-// {
-// 	size_t	i;
-// 	size_t	j;
-
-// 	i = 0;
-// 	while (buffer[i] && buffer[i] != '\n')
-// 		i++;
-// 	if (buffer[i] == '\n')
-// 		i++;
-// 	j = 0;
-// 	if (!buffer[i])
-// 	{
-// 		ft_bzero(&buffer[j], BUFFER_SIZE - j);
-// 		return ;
-// 	}
-// 	while (buffer[i + j])
-// 	{
-// 		buffer[j] = buffer[i + j];
-// 		j++;
-// 	}
-// 	ft_bzero(&buffer[j], BUFFER_SIZE - j);
-// }
-
-// char	*get_next_line(int fd)
-// {
-// 	static char	buffer[BUFFER_SIZE + 1];
-// 	char		*line;
-// 	size_t		bytes_read;
-
-// 	if (fd == -1 || BUFFER_SIZE <= 0)
-// 		return (NULL);
-// 	line = malloc(sizeof(char) * 1);
-// 	if (!line)
-// 		return (free(line), NULL);
-// 	line[0] = '\0';
-// 	if (ft_strlen(buffer) >= 1)
-// 		line = ft_strjoin(line, buffer);
-// 	while (!ft_strchr(buffer, '\n'))
-// 	{
-// 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-// 		if (bytes_read <= 0)
-// 			return (free(line), ft_bzero(buffer, ft_strlen(buffer)), NULL);
-// 		line = ft_strjoin(line, buffer);
-// 	}
-// 	get_leftover(buffer);
-// 	return (line);
-// }
 
 // int	main(void)
 // {
 // 	int	fd;
 
-// 	fd = open("1char.txt", O_RDONLY);
+// 	fd = open("1char.txt", O_RDONLY );
 // 	// fd = open("holawenas.txt", O_RDONLY);
-// 	printf("FD=>%d", fd);
+// 	//printf("FD=>%d\n", fd);
 // 	printf("LINEA1=>%s", get_next_line(fd));
-// 	printf("FD=>%d", fd);
+// 	//printf("FD=>%d\n", fd);
 // 	printf("LINEA2=>%s", get_next_line(fd));
-// 	printf("FD=>%d", fd);
-// 	printf("LINEA3=>%s", get_next_line(fd));
-// 	printf("FD=>%d", fd);
-// 	printf("LINEA4=>%s", get_next_line(fd));
-// 	printf("FD=>%d", fd);
-// 	printf("LINEA5=>%s", get_next_line(fd));
-// 	printf("FD=>%d", fd);
-// 	printf("LINEA6=>%s", get_next_line(fd));
+// 	// printf("FD=>%d", fd);
+// 	// printf("LINEA3=>%s", get_next_line(fd));
+// 	// printf("FD=>%d", fd);
+// 	// printf("LINEA4=>%s", get_next_line(fd));
+// 	// printf("FD=>%d", fd);
+// 	// printf("LINEA5=>%s", get_next_line(fd));
+// 	// printf("FD=>%d", fd);
+// 	// printf("LINEA6=>%s", get_next_line(fd));
 // 	close(fd);
 // 	return (0);
 // }
